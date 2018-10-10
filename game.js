b62cda297dbb02cddfef28b63100e104671a929c
Paul Vangelakos <paulvangelakos@gmail.com>
2018-10-10 15:54:17 -0600

    Creating the structure of the game class

---
+++ game.js
@@ -0,0 +1,29 @@
+class Game {
+  constructor(player, round, winner) {
+    this.player = [];
+    this.round = round;
+    this.winner = winner;
+  }
+
+
+  startGame() {
+
+  }
+
+  resetGame() {
+
+  }
+
+  updateRound() {
+
+  }
+
+  declareWinner() {
+
+  }
+
+}
+
+
+
+
