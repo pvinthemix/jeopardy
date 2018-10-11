3304d0a424403acb7532f744b158e2a498781994
Paul Vangelakos <paulvangelakos@gmail.com>
2018-10-11 14:06:12 -0600

    Added basic first test to the game-class test file

--- game.js
+++ game.js
@@ -26,4 +26,5 @@ class Game {
 
 
 
+module.exports = Game;
 
--- test/game-test.js
+++ test/game-test.js
@@ -3,15 +3,15 @@ const expect = chai.expect;
 const Game = require('../game.js');
 
 
-describe('Box', function() {
+describe('Game', function() {
   var game;
 
   beforeEach(function() {
     game = new Game();
-    // runs before all tests in this block
   });
 
   it ('should return true', function() {
 
     expect(true).to.equal(true);
   });
+});
